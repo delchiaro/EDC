ecefa5fd35ef76be75eb7573bb0a3a4186882fd5
Nagash <nagash@nagash-laptop-ubuntu.(none)>
2010-05-06 19:30:02 +0200

    Pulito completamente il codice 'esternamente', cioè creati tutti i file .h/.c, makefile completo e decente (creato con proprietà del progetto netbeans), risistemate la posizione delle funzioni ecc. Changelog: - Creato eibnetmux.c - Creato eibnetmux.h - Eliminate le variabili globali di eibnetmux.c, inserite nel main dove venivano utilizzate. - NO WARNING!! Compilazione pulita al 100%!!! :D - Modificato il readme. - Passaggio ufficiale a netbeans 6.8!
    Todo:
    - Abbiamo un codice abbastanza pulito, dobbiamo solo smaltire le funzioni rendendole più comprensibili ed eliminando le routine inutili!
    - Parametrizzazione da file.
    - Parametrizzazione da linea di comando.
    
    ! E D C !

--- src/eibtrace.c
+++ src/eibtrace.c
@@ -1,41 +1,9 @@
-
-#ifdef HAVE_CONFIG_H
-#include "../config.h"
-#endif
-
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <libgen.h>
-#include <getopt.h>
-#include <errno.h>
-#include <time.h>
-#include <math.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-
-#include <eibnetmux/enmx_lib.h>
-//#include "../mylib/mylib.h"
-#include "mylib.h"
-
-
-
-#define EIB_DAF_GROUP                   0x80
-#define A_RESPONSE_VALUE_REQ            0x0040
-#define A_WRITE_VALUE_REQ               0x0080
-
-
-// Global variables
-ENMX_HANDLE     sock_con = 0;
-unsigned char   conn_state = 0;
-
-
 #include "eibtrace.h"
 
 
-static void Usage( char *progname )
+
+
+void Usage( char *progname )
 {
     fprintf( stderr, "Usage: %s [options] [hostname[:port]]\n"
                      "where:\n"
@@ -51,10 +19,11 @@ static void Usage( char *progname )
 
 
 
+
 /*
  * Return representation of physical device KNX address as string
  */
-static char *knx_physical( uint16_t phy_addr )
+char *knx_physical( uint16_t phy_addr )
 {
         static char     textual[64];
         int             area;
@@ -72,10 +41,11 @@ static char *knx_physical( uint16_t phy_addr )
 }
 
 
+
 /*
  * Return representation of logical KNX group address as string
  */
-static char *knx_group( uint16_t grp_addr )
+char *knx_group( uint16_t grp_addr )
 {
         static char     textual[64];
         int             top;
@@ -91,4 +61,6 @@ static char *knx_group( uint16_t grp_addr )
         return( textual );
 }
 
+
+//END OF EIBTRACE.C
 
--- src/eibtrace.h
+++ src/eibtrace.h
@@ -1,6 +1,46 @@
-static void     Usage( char *progname );
-static char     *knx_physical( uint16_t phy_addr );
-static char     *knx_group( uint16_t grp_addr );
+/* 
+ * File:   eibtrace.h
+ * Author: nagash
+ *
+ * Created on 6 maggio 2010, 19.03
+ */
+
+#ifndef _EIBTRACE_H
+#define	_EIBTRACE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    #ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <libgen.h>
+#include <getopt.h>
+#include <errno.h>
+#include <time.h>
+#include <math.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+#include <eibnetmux/enmx_lib.h>
+
+#define EIB_DAF_GROUP                   0x80
+#define A_RESPONSE_VALUE_REQ            0x0040
+#define A_WRITE_VALUE_REQ               0x0080
+
+
+// Global variables
+
+void     Usage( char *progname );
+char     *knx_physical( uint16_t phy_addr );
+char     *knx_group( uint16_t grp_addr );
 
 /*
  * EIB request frame
@@ -17,3 +57,11 @@ typedef struct __attribute__((packed)) {
         uint8_t  apci;
         uint8_t  data[16];
 } CEMIFRAME;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _EIBTRACE_H */
+
